58dea03de901af6ddcff46aea01c68a412ebdd61
Kareem El-Houshy <kareemel@ad.unc.edu>
2025-01-22 12:26:20 -0500

    second backup of tea_party

--- exercises/ex01_tea_party.py
+++ exercises/ex01_tea_party.py
@@ -1,15 +1,20 @@
 """Program to determine number of treats, tea bags, and expected cost of a tea party 
 based on an inputted number of guests"""
 
-_author_: str = "730482808"
+__author__: str = "730482808"
 
 
 def main_planner(guests: int):
     """Prints number of teas, treats, and expected cost of the tea party"""
     print("A Cosy Tea Party for " + str(guests) + " People!")
-    print("Tea Bags: " + str(tea_bags))
-    print("Treats: " + str(treats))
-    print("Cost: $" + str(cost))
+    print("Tea Bags: " + str(tea_bags(people=guests)))
+    print("Treats: " + str(treats(people=guests)))
+    print(
+        "Cost: $"
+        + str(
+            cost(tea_count=tea_bags(people=guests), treat_count=treats(people=guests))
+        )
+    )
     return None
 
 
@@ -20,7 +25,7 @@ def tea_bags(people: int) -> int:
 
 def treats(people: int) -> int:
     """Returns number of treats needed based on number of guests"""
-    return int(tea_bags(people) * 1.5)
+    return int(tea_bags(people=people) * 1.5)
 
 
 def cost(tea_count: int, treat_count: int) -> float:
@@ -28,5 +33,5 @@ def cost(tea_count: int, treat_count: int) -> float:
     return tea_count * 0.5 + treat_count * 0.75
 
 
-if __name__ == "_main_":
+if __name__ == "__main__":
     main_planner(guests=int(input("How many guests are attending your tea party? ")))
